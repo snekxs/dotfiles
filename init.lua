bd734c037bfefc50f35d35a8b62447888074918f
snekxs <snekxs@oblivyx.com>
2026-02-21 01:50:28 -0700

    first commit

---
+++ init.lua
@@ -0,0 +1,41 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+   {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' }
+},{'folke/tokyonight.nvim'},{'vyfor/cord.nvim'},{"webhooked/kanso.nvim"},{"neovim/nvim-lspconfig"},
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = {  },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+  }})
+
+require('lualine').setup{options = { theme = 'iceberg_dark'}}
+vim.cmd[[colorscheme kanso-ink]]
+require('cord').setup {{enabled=true},display={theme='catppuccin',flavor='accent'}}
+
